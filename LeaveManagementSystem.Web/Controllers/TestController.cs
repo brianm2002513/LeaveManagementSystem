fe1d938b6e057b986e1b391d6bb4a7dd223104a2
munas <munas@BRIAN_MUNASHE>
2025-05-09 23:43:01 +0200

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Brian Mbawa",
+                DateOfBirth = new DateTime(2002, 12, 26)
+            };
+
+            return View(data);
+        }
+    }
+}
